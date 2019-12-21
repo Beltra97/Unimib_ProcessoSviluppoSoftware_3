80d79623523624c781b4fbf2ea77bb698455249c
Davide Finati <d.finati@campus.unimib.it>
2019-12-21 11:49:17 +0100

    Aggiunto try catch per transaction rollback

--- worksocialmedia/src/main/java/worksocialmedia/repository/JobRepositoryImpl.java
+++ worksocialmedia/src/main/java/worksocialmedia/repository/JobRepositoryImpl.java
@@ -11,50 +11,54 @@ import worksocialmedia.model.Job;
 
 public class JobRepositoryImpl implements JobRepository {
 
-  private EntityManagerFactory entityManagerFactory;
+	private EntityManagerFactory entityManagerFactory;
 
-  public JobRepositoryImpl() {
-    this.entityManagerFactory = Persistence.createEntityManagerFactory("worksocialmedia");
-  }
+	public JobRepositoryImpl() {
+		this.entityManagerFactory = Persistence.createEntityManagerFactory("worksocialmedia");
+	}
 
-  @Override
-  public Optional<Job> findById(Long id) {
-    final EntityManager entityManager = this.entityManagerFactory.createEntityManager();
-    Job job = entityManager.find(Job.class, id);
-    entityManager.close();
-    return Optional.ofNullable(job);
-  }
-
-  @Override
-  public Iterable<Job> findAll() {
-    final EntityManager entityManager = this.entityManagerFactory.createEntityManager();
-    List<Job> jobs = entityManager.createQuery("FROM Job", Job.class).getResultList();
-    entityManager.close();
-    return jobs;
-  }
-  
-  public void deleteJobById(Long id) {
+	@Override
+	public Optional<Job> findById(Long id) {
 		final EntityManager entityManager = this.entityManagerFactory.createEntityManager();
-		if (!entityManager.getTransaction().isActive()) {
-			entityManager.getTransaction().begin();
-		}
 		Job job = entityManager.find(Job.class, id);
-		entityManager.remove(job);
-		entityManager.getTransaction().commit();
 		entityManager.close();
-	  }
-  
-  public void addJob(Job job) {
+		return Optional.ofNullable(job);
+	}
+
+	@Override
+	public Iterable<Job> findAll() {
+		final EntityManager entityManager = this.entityManagerFactory.createEntityManager();
+		List<Job> jobs = entityManager.createQuery("FROM Job", Job.class).getResultList();
+		entityManager.close();
+		return jobs;
+	}
+
+	public void deleteJobById(Long id) {
+		final EntityManager entityManager = this.entityManagerFactory.createEntityManager();
+		try {
+			if (!entityManager.getTransaction().isActive()) {
+				entityManager.getTransaction().begin();
+			}
+			Job job = entityManager.find(Job.class, id);
+			entityManager.remove(job);
+			entityManager.getTransaction().commit();
+			entityManager.close();
+		} catch (Exception e) {
+			entityManager.getTransaction().rollback();
+		}
+	}
+
+	public void addJob(Job job) {
 		final EntityManager entityManager = this.entityManagerFactory.createEntityManager();
 		if (!entityManager.getTransaction().isActive()) {
 			entityManager.getTransaction().begin();
 		}
-		entityManager.persist(job);	
+		entityManager.persist(job);
 		entityManager.getTransaction().commit();
 		entityManager.close();
-	  }
-  
-  public void updateJob(Long id, String name, String category, String description) {
+	}
+
+	public void updateJob(Long id, String name, String category, String description) {
 		final EntityManager entityManager = this.entityManagerFactory.createEntityManager();
 		if (!entityManager.getTransaction().isActive()) {
 			entityManager.getTransaction().begin();
@@ -66,16 +70,17 @@ public class JobRepositoryImpl implements JobRepository {
 		entityManager.persist(job);
 		entityManager.getTransaction().commit();
 		entityManager.close();
-	  }
-  
-  public Job searchJob(String jobSearchName) {
+	}
+
+	public Job searchJob(String jobSearchName) {
 		final EntityManager entityManager = this.entityManagerFactory.createEntityManager();
-		  
-		Job job = (Job) entityManager.createQuery("FROM Job j WHERE j.name = '" + jobSearchName + "'").getSingleResult();
+
+		Job job = (Job) entityManager.createQuery("FROM Job j WHERE j.name = '" + jobSearchName + "'")
+				.getSingleResult();
 
 		entityManager.close();
-		  
+
 		return job;
-	  }
+	}
 
 }
